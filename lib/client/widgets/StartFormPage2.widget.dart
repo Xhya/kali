b6a14646b1b8b782972908cedf6cea7cc6aef0e6
alexiasouvane <alexia.souvane@gmail.com>
2025-07-20 17:00:36 +0200

    start form page 2

--- lib/client/widgets/StartFormPage2.widget.dart
+++ lib/client/widgets/StartFormPage2.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kali/client/widgets/DateInput.widget.dart';
 import 'package:kali/core/states/startForm.state.dart';
 import 'package:provider/provider.dart';
 import 'package:kali/client/Style.service.dart';
@@ -14,20 +15,51 @@ class StartFormPage2 extends StatefulWidget {
 class _StartFormPage2State extends State<StartFormPage2> {
   @override
   Widget build(BuildContext context) {
-    SelectOption? genderOption = context.watch<StartFormState>().genderOption.value;
+    SelectOption? genderOption =
+        context.watch<StartFormState>().genderOption.value;
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 12),
             Text(
-              "Rentrons un peu dans les détails",
+              "Ton profil santé 🩺",
               style: style.text.neutral.merge(style.fontsize.lg),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 4),
+            Text(
+              "Ces infos nous aident à calculer ton métabolisme de base avec précision.",
+              style: style.text.neutral.merge(style.fontsize.xs),
+            ),
+
+            SizedBox(height: 44),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                "Ta date de naissance",
+                style: style.text.neutral.merge(style.fontsize.sm),
+              ),
+            ),
+            DateInputWidget(
+              onUpdateDate: (String value) {
+                // startFormState.birthdate.value = value;
+              },
+            ),
+
+            SizedBox(height: 32),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                "Ton sexe",
+                style: style.text.neutral.merge(style.fontsize.sm),
+              ),
+            ),
+
             CustomSelectWidget(
               onChanged: (SelectOption? value) {
                 startFormState.genderOption.value = value;
