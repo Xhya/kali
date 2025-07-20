9d6036126ae46d594dec9172ff25ad18fd12bde1
alexiasouvane <alexia.souvane@gmail.com>
2025-07-20 18:10:16 +0200

    refactor start form

--- lib/client/screens/StartForm.screen.dart
+++ lib/client/screens/StartForm.screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:kali/client/widgets/MainButton.widget.dart';
 import 'package:kali/client/widgets/StartFormPage1.widget.dart';
 import 'package:kali/client/widgets/StartFormPage2.widget.dart';
 import 'package:kali/client/widgets/StartFormPage3.widget.dart';
 import 'package:kali/client/widgets/StartFormPage4.widget.dart';
+import 'package:kali/client/widgets/StartFormPage5.widget.dart';
+import 'package:kali/client/widgets/StartFormPageFinal.widget.dart';
 import 'package:kali/client/widgets/StartFormTop.widget.dart';
 import 'package:kali/core/models/NutriScore.model.dart';
 import 'package:provider/provider.dart';
 import 'package:kali/client/Style.service.dart';
-import 'package:kali/client/widgets/CustomButton.widget.dart';
 import 'package:kali/client/layout/Base.scaffold.dart';
 import 'package:kali/core/actions/startForm.actions.dart';
 import 'package:kali/core/states/nutriScore.state.dart';
@@ -78,90 +80,72 @@ class _StartFormScreenState extends State<StartFormScreen> {
     return BaseScaffold(
       child: Scaffold(
         backgroundColor: style.background.greenTransparent.color,
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Column(
-                children: [
-                  StartFormTopWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StartFormTopWidget(),
 
-                  Expanded(
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                        minHeight: constraints.maxHeight,
-                      ),
-                      child: IntrinsicHeight(
-                        child: PageView.builder(
-                          controller: controller,
-                          itemCount: 7,
-                          physics: NeverScrollableScrollPhysics(),
-                          onPageChanged: (index) {
-                            // TODO
-                          },
-                          itemBuilder: (_, index) {
-                            switch (index) {
-                              case 1:
-                                return StartFormPage1();
-                              case 1:
-                                return StartFormPage2();
-                              case 0:
-                                return StartFormPage3();
-                              case 3:
-                                return StartFormPage4();
-                              default:
-                                return StartFormPage1();
-                            }
-                          },
-                        ),
-                      ),
-                    ),
+              Expanded(
+                child: IntrinsicHeight(
+                  child: PageView.builder(
+                    controller: controller,
+                    itemCount: 6,
+                    physics: NeverScrollableScrollPhysics(),
+                    onPageChanged: (index) {
+                      // TODO
+                    },
+                    itemBuilder: (_, index) {
+                      switch (index) {
+                        case 0:
+                          return StartFormPage1();
+                        case 1:
+                          return StartFormPage2();
+                        case 2:
+                          return StartFormPage3();
+                        case 3:
+                          return StartFormPage4();
+                        case 4:
+                          return StartFormPage5();
+                        case 5:
+                          return StartFormPageFinal();
+                        default:
+                          return StartFormPage1();
+                      }
+                    },
                   ),
-
-                  // ButtonWidget(
-                  //   text: "Crash",
-                  //   onPressed: () {
-                  //     errorService.notifyError(
-                  //       e: Exception("Crash button pressed"),
-                  //     );
-                  //   },
-                  //   fullWidth: true,
-                  // ),
-                  // if (personalNutriScore != null)
-                  //   ExpandedWidget(
-                  //     child: Wrap(
-                  //       spacing: 12,
-                  //       runSpacing: 12,
-                  //       children: [
-                  //         NutriScore2by2Widget(nutriScore: personalNutriScore),
-                  //       ],
-                  //     ),
-                  //   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: ButtonWidget(
-                      text: "Suivant",
-                      onPressed: () {
-                        onClickNext();
-                      },
-                      fullWidth: true,
-                    ),
-                  ),
-
-                  // ButtonWidget(
-                  //   text: getSubmitButtonText(),
-                  //   onPressed: () {
-                  //     onClickSubmitButton();
-                  //   },
-                  //   fullWidth: true,
-                  //   disabled: isSubmitButtonDisabled,
-                  //   isLoading: isLoading,
-                  // ),
-                  SizedBox(height: 16),
-                ],
+                ),
               ),
-            );
+
+              // ButtonWidget(
+              //   text: "Crash",
+              //   onPressed: () {
+              //     errorService.notifyError(
+              //       e: Exception("Crash button pressed"),
+              //     );
+              //   },
+              //   fullWidth: true,
+              // ),
+              // if (personalNutriScore != null)
+              //   ExpandedWidget(
+              //     child: Wrap(
+              //       spacing: 12,
+              //       runSpacing: 12,
+              //       children: [
+              //         NutriScore2by2Widget(nutriScore: personalNutriScore),
+              //       ],
+              //     ),
+              //   ),
+            ],
+          ),
+        ),
+        floatingActionButton: MainButtonWidget(
+          onClick: () {
+            onClickNext();
           },
+          text: "suivant",
+          iconWidget: Icon(Icons.arrow_forward, size: 20),
         ),
       ),
     );
