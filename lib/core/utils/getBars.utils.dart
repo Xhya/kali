ee262842432f1788394b96d481afe93e118c9829
alexiasouvane <alexia.souvane@gmail.com>
2025-07-28 20:49:52 +0200

    refactor horizontal bar

--- lib/core/utils/getBars.utils.dart
+++ lib/core/utils/getBars.utils.dart
@@ -7,7 +7,10 @@ import 'package:kali/core/models/Meal.model.dart';
 import 'package:kali/core/models/MealPeriod.enum.dart';
 import 'package:kali/core/utils/getTotalNutriscoreByPeriod.utils.dart';
 
-Map<Color, int> getBars(List<MealModel> mealsByPeriods, MacroTypeEnum macroType) {
+Map<Color, int> getBars(
+  List<MealModel> mealsByPeriods,
+  MacroTypeEnum macroType,
+) {
   NutriScoreByPeriod dateTotalNutriscoreByPeriod = getTotalNutriscoreByPeriod(
     mealsByPeriods,
   );
@@ -60,66 +63,87 @@ Map<Color, int> getBars(List<MealModel> mealsByPeriods, MacroTypeEnum macroType)
   }
 
   if (macroType == MacroTypeEnum.proteins) {
+    final proteinsBreakfast =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.proteinAmount;
+    final proteinsLunch =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.proteinAmount;
+    final proteinsSnack =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.proteinAmount;
+    final proteinsDinner =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.proteinAmount;
+
     return <Color, int>{
-      if (selectedPeriods.contains(MealPeriodEnum.breakfast))
-        style.period.breakfastColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!
-                .proteinAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.lunch))
-        style.period.lunchColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.proteinAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.snack))
-        style.period.snackColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.proteinAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.dinner))
-        style.period.dinerColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.proteinAmount,
+      if (selectedPeriods.contains(MealPeriodEnum.breakfast) &&
+          proteinsBreakfast > 0)
+        style.period.breakfastColor.color!: proteinsBreakfast,
+      if (selectedPeriods.contains(MealPeriodEnum.lunch) && proteinsLunch > 0)
+        style.period.lunchColor.color!: proteinsLunch,
+      if (selectedPeriods.contains(MealPeriodEnum.snack) && proteinsSnack > 0)
+        style.period.snackColor.color!: proteinsSnack,
+      if (selectedPeriods.contains(MealPeriodEnum.dinner) && proteinsDinner > 0)
+        style.period.dinerColor.color!: proteinsDinner,
     };
   }
 
   if (macroType == MacroTypeEnum.glucids) {
+    final glucidsBreakfast =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.glucidAmount;
+    final glucidsLunch =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.glucidAmount;
+    final glucidsSnack =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.glucidAmount;
+    final glucidsDinner =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.glucidAmount;
+
     return <Color, int>{
-      if (selectedPeriods.contains(MealPeriodEnum.breakfast))
-        style.period.breakfastColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.glucidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.lunch))
-        style.period.lunchColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.glucidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.snack))
-        style.period.snackColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.glucidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.dinner))
-        style.period.dinerColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.glucidAmount,
+      if (selectedPeriods.contains(MealPeriodEnum.breakfast) &&
+          glucidsBreakfast > 0)
+        style.period.breakfastColor.color!: glucidsBreakfast,
+      if (selectedPeriods.contains(MealPeriodEnum.lunch) && glucidsLunch > 0)
+        style.period.lunchColor.color!: glucidsLunch,
+      if (selectedPeriods.contains(MealPeriodEnum.snack) && glucidsSnack > 0)
+        style.period.snackColor.color!: glucidsSnack,
+      if (selectedPeriods.contains(MealPeriodEnum.dinner) && glucidsDinner > 0)
+        style.period.dinerColor.color!: glucidsDinner,
     };
   }
 
   if (macroType == MacroTypeEnum.lipids) {
+    final lipidsBreakfast =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.lipidAmount;
+    final lipidsLunch =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.lipidAmount;
+    final lipidsSnack =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.lipidAmount;
+    final lipidsDinner =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.lipidAmount;
+
     return <Color, int>{
-      if (selectedPeriods.contains(MealPeriodEnum.breakfast))
-        style.period.breakfastColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.lipidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.lunch))
-        style.period.lunchColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.lipidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.snack))
-        style.period.snackColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.lipidAmount,
-      if (selectedPeriods.contains(MealPeriodEnum.dinner))
-        style.period.dinerColor.color!:
-            dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.lipidAmount,
+      if (selectedPeriods.contains(MealPeriodEnum.breakfast) &&
+          lipidsBreakfast > 0)
+        style.period.breakfastColor.color!: lipidsBreakfast,
+      if (selectedPeriods.contains(MealPeriodEnum.lunch) && lipidsLunch > 0)
+        style.period.lunchColor.color!: lipidsLunch,
+      if (selectedPeriods.contains(MealPeriodEnum.snack) && lipidsSnack > 0)
+        style.period.snackColor.color!: lipidsSnack,
+      if (selectedPeriods.contains(MealPeriodEnum.dinner) && lipidsDinner > 0)
+        style.period.dinerColor.color!: lipidsDinner,
     };
   }
 
   if (macroType == MacroTypeEnum.calories) {
-    
-    final caloryBreakfast = dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.caloryAmount;
-    final caloryLunch = dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.caloryAmount;
-    final calorySnack = dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.caloryAmount;
-    final caloryDinner = dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.caloryAmount;
+    final caloryBreakfast =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.breakfast]!.caloryAmount;
+    final caloryLunch =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.lunch]!.caloryAmount;
+    final calorySnack =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.snack]!.caloryAmount;
+    final caloryDinner =
+        dateTotalNutriscoreByPeriod[MealPeriodEnum.dinner]!.caloryAmount;
 
     return <Color, int>{
-      if (selectedPeriods.contains(MealPeriodEnum.breakfast) && caloryBreakfast > 0)
+      if (selectedPeriods.contains(MealPeriodEnum.breakfast) &&
+          caloryBreakfast > 0)
         style.period.breakfastColor.color!: caloryBreakfast,
       if (selectedPeriods.contains(MealPeriodEnum.lunch) && caloryLunch > 0)
         style.period.lunchColor.color!: caloryLunch,
