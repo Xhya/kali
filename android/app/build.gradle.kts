4a67a18c230e5acd8bd83ac694f98e8586d91333
alexiasouvane <alexia.souvane@gmail.com>
2025-08-28 15:48:14 +0200

    upgrade flutter build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,12 +28,14 @@ android {
     }
 
     defaultConfig {
+        val minSdkVersionFromEnv = 23
+
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.horace.kali"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = minSdkVersionFromEnv
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
