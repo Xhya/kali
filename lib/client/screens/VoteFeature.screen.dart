07ab52a7ebad7b63a247b422b31e6477bdaa4051
alexiasouvane <alexia.souvane@gmail.com>
2025-09-13 09:54:20 +0200

    imprve feature display

--- lib/client/screens/VoteFeature.screen.dart
+++ lib/client/screens/VoteFeature.screen.dart
@@ -1,5 +1,7 @@
 // ignore: depend_on_referenced_packages
 import 'package:flutter/material.dart';
+import 'package:kali/client/Style.service.dart';
+import 'package:kali/client/widgets/CustomCard.widget.dart';
 import 'package:kali/client/widgets/LoaderIcon.widget.dart';
 import 'package:kali/core/models/Feature.model.dart';
 import 'package:provider/provider.dart';
@@ -46,22 +48,59 @@ class _VoteFeatureScreenState extends State<VoteFeatureScreen> {
 
     return BaseScaffold(
       backButton: true,
-      child: Column(
-        children: [
-          if (isLoadingFeatures) LoaderIcon(),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            if (isLoadingFeatures) LoaderIcon(),
 
-          if (!isLoadingFeatures)
-            Expanded(
-              child: ListView.separated(
-                itemCount: features.length,
-                separatorBuilder: (context, index) => SizedBox(height: 4),
-                itemBuilder: (BuildContext context, int index) {
-                  final feature = features[index];
-                  return Text(feature.name);
-                },
+            if (!isLoadingFeatures)
+              Expanded(
+                child: ListView.separated(
+                  itemCount: features.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    final feature = features[index];
+                    return CustomCard(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 12,
+                      ),
+                      child: Column(
+                        spacing: 8,
+                        children: [
+                          Row(
+                            spacing: 16,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  feature.name,
+                                  style: style.fontsize.md.merge(
+                                    style.fontweight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                ),
+                              ),
+                              Icon(Icons.heart_broken_outlined),
+                            ],
+                          ),
+
+                          if (feature.description != null)
+                            Text(
+                              feature.description!,
+                              style: style.fontsize.sm,
+                            ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
