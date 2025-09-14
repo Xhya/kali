639108afdc0b40ec45f0b8fdb55b70991089733f
alexiasouvane <alexia.souvane@gmail.com>
2025-09-14 09:49:33 +0200

    center login widget

--- lib/client/widgets/Login.widget.dart
+++ lib/client/widgets/Login.widget.dart
@@ -57,103 +57,106 @@ class _LoginWidgetState extends State<LoginWidget> {
 
     return Padding(
       padding: EdgeInsets.all(12),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Text(
-            t('nice_to_see_you_again'),
-            style: style.fontsize.lg
-                .merge(style.text.neutral)
-                .merge(style.fontweight.bold),
-            textAlign: TextAlign.start,
-          ),
-          SizedBox(height: 4),
-          Text(
-            t('time_to_get_back'),
-            style: style.fontsize.sm.merge(style.text.neutralLight),
-            textAlign: TextAlign.start,
-          ),
-          SizedBox(height: 32),
-          EmailInputWidget(),
-          SizedBox(height: 4),
-          PasswordInputWidget(),
-          SizedBox(height: 4),
-          if (error != null)
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                registerState.error.value!,
-                style: style.text.error.merge(style.fontsize.sm),
-              ),
-            ),
-          SizedBox(height: 4),
-          GestureDetector(
-            onTap: () async {
-              navigationService.navigateBack();
-              await Future.delayed(Duration(milliseconds: 500));
-              registerState.error.value = null;
-              navigationService.openBottomSheet(
-                widget: WelcomeBottomSheet(child: ForgotPasswordWidget()),
-              );
-            },
-            child: Text(
-              t('i_forgot_password'),
-              style: style.fontsize.sm
+      child: Center(child:  SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              t('nice_to_see_you_again'),
+              style: style.fontsize.lg
                   .merge(style.text.neutral)
-                  .merge(
-                    TextStyle(
-                      decoration: TextDecoration.underline,
-                      decorationColor: style.text.neutral.color,
-                    ),
-                  ),
+                  .merge(style.fontweight.bold),
+              textAlign: TextAlign.start,
             ),
-          ),
-          SizedBox(height: 32),
-          Flex(
-            direction: Axis.horizontal,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MainButtonWidget(
-                onClick: () {
-                  onSubmitLogin();
-                },
-                text: t('connect'),
-                iconWidget: Icon(Icons.arrow_forward, size: 20),
-                disabled: false,
-                isLoading: false,
+            SizedBox(height: 4),
+            Text(
+              t('time_to_get_back'),
+              style: style.fontsize.sm.merge(style.text.neutralLight),
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(height: 32),
+            EmailInputWidget(),
+            SizedBox(height: 4),
+            PasswordInputWidget(),
+            SizedBox(height: 4),
+            if (error != null)
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Text(
+                  registerState.error.value!,
+                  style: style.text.error.merge(style.fontsize.sm),
+                ),
               ),
-            ],
-          ),
-          SizedBox(height: 40),
-          // Row(
-          //   spacing: 8,
-          //   children: [
-          //     Expanded(
-          //       child: Container(
-          //         decoration: BoxDecoration(
-          //           border: Border.all(color: style.border.color.color2.color!),
-          //         ),
-          //       ),
-          //     ),
-          //     Text(
-          //       "Ou se connecter via",
-          //       style: style.fontsize.sm.merge(style.text.neutralLight),
-          //       textAlign: TextAlign.center,
-          //     ),
-          //     Expanded(
-          //       child: Container(
-          //         decoration: BoxDecoration(
-          //           border: Border.all(color: style.border.color.color2.color!),
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // ),
-          // SizedBox(height: 12),
-          // GoogleSignInButton(action: 'login'),
-        ],
+            SizedBox(height: 4),
+            GestureDetector(
+              onTap: () async {
+                navigationService.navigateBack();
+                await Future.delayed(Duration(milliseconds: 500));
+                registerState.error.value = null;
+                navigationService.openBottomSheet(
+                  widget: WelcomeBottomSheet(child: ForgotPasswordWidget()),
+                );
+              },
+              child: Text(
+                t('i_forgot_password'),
+                style: style.fontsize.sm
+                    .merge(style.text.neutral)
+                    .merge(
+                      TextStyle(
+                        decoration: TextDecoration.underline,
+                        decorationColor: style.text.neutral.color,
+                      ),
+                    ),
+              ),
+            ),
+            SizedBox(height: 32),
+            Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MainButtonWidget(
+                  onClick: () {
+                    onSubmitLogin();
+                  },
+                  text: t('connect'),
+                  iconWidget: Icon(Icons.arrow_forward, size: 20),
+                  disabled: false,
+                  isLoading: false,
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            // Row(
+            //   spacing: 8,
+            //   children: [
+            //     Expanded(
+            //       child: Container(
+            //         decoration: BoxDecoration(
+            //           border: Border.all(color: style.border.color.color2.color!),
+            //         ),
+            //       ),
+            //     ),
+            //     Text(
+            //       "Ou se connecter via",
+            //       style: style.fontsize.sm.merge(style.text.neutralLight),
+            //       textAlign: TextAlign.center,
+            //     ),
+            //     Expanded(
+            //       child: Container(
+            //         decoration: BoxDecoration(
+            //           border: Border.all(color: style.border.color.color2.color!),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // SizedBox(height: 12),
+            // GoogleSignInButton(action: 'login'),
+          ],
+        ),
+        ),
       ),
     );
   }
